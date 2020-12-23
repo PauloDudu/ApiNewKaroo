20a9f02d37f58f01795deccf49e0e5323a06f664
dsnpack <dsnpack@PAT2735.alterdata.matriz>
2020-12-23 14:07:30 -0300

    Updating

---
+++ karoo/src/test/java/br/com/dsn/pack/karoo/ApiNewKarooApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dsn.pack.karoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNewKarooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
