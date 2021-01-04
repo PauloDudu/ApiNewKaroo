1ace03e81827bd999b1bd4ada7dfc580e1bb41a3
dsnpack <dsnpack@PAT2735.alterdata.matriz>
2021-01-04 10:04:56 -0300

    Changing files to deploy

---
+++ src/main/java/br/com/dsn/pack/karoo/controller/DestaqueController.java
@@ -0,0 +1,63 @@
+package br.com.dsn.pack.karoo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.dsn.pack.karoo.domain.Destaque;
+import br.com.dsn.pack.karoo.exceptions.DestaqueNotFoundException;
+import br.com.dsn.pack.karoo.exceptions.DestaqueNotValidException;
+import br.com.dsn.pack.karoo.service.DestaqueService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@RequestMapping("/karoo")
+@Api(value="Api REST Alterdata newKaroo")
+@CrossOrigin(origins="*")
+public class DestaqueController {
+	
+	@Autowired
+	DestaqueService destaqueService;
+	
+	@GetMapping("/destaques")
+	@ApiOperation(value="Retorna uma lista de usuarios")
+	public ResponseEntity<List<Destaque>> listUsers() throws DestaqueNotFoundException {
+		return ResponseEntity.status(HttpStatus.OK).body(destaqueService.getAll());
+	}
+	
+	@GetMapping("/destaques/{id}")
+	@ApiOperation(value="Retorna um usuario pelo id passado na rota")
+	public ResponseEntity<Destaque> getById(@PathVariable(value="id") long id) throws DestaqueNotFoundException {
+		return ResponseEntity.status(HttpStatus.OK).body(destaqueService.getById(id));
+	}
+	
+	@PostMapping("/destaques")
+	@ApiOperation(value="Adiciona um usuario a lista")
+	public ResponseEntity<?> createUser(@RequestBody Destaque destaque) throws DestaqueNotValidException {
+		return ResponseEntity.status(HttpStatus.OK).body(destaqueService.addDestaque(destaque)); 
+	}
+	
+	@DeleteMapping("/destaques/{id}")
+	@ApiOperation(value="Deleta um usuario da lista")
+	public void deleteUser(@PathVariable(value="id") long id) throws DestaqueNotFoundException {
+		destaqueService.deleteDestaque(id);
+	}
+	
+	@PutMapping("/destaques")
+	@ApiOperation(value="Atualiza um usuario")
+	public ResponseEntity<Destaque> updateUser(@RequestBody Destaque destaque) throws DestaqueNotValidException {
+		return ResponseEntity.ok(destaqueService.updateDestaque(destaque));
+	}
+}
---
+++ src/main/java/br/com/dsn/pack/karoo/domain/Funcionario.java
@@ -0,0 +1,68 @@
+package br.com.dsn.pack.karoo.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="funcionario")
+public class Funcionario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;	
+	
+	private String email;	
+	
+	private String nome;
+	
+	private String senha;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public boolean ehvalido() {
+		boolean ehValido = false;
+		
+		if(this.email.isEmpty() || this.nome.isEmpty() || this.senha.isEmpty()) {
+			ehValido = true;
+		}
+		
+		return ehValido;
+	}
+}
